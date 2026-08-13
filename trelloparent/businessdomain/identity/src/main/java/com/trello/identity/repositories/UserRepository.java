08bed46fc3d60796994b03b8827bc42fd3bcd993
Armando KD <enrique1010k@gmail.com>
2026-08-12 21:26:32 -0500

    Crear una base de datos PostgreSQL desde Docker

---
+++ trelloparent/businessdomain/identity/src/main/java/com/trello/identity/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.trello.identity.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trello.identity.entities.User;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
